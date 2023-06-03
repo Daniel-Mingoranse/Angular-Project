de8c750a0f23065d8013689c15934693648ca495
CintiaKarolliny <79120035+CintiaKarolliny@users.noreply.github.com>
2023-06-03 06:53:33 -0300

    feat: listagem de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { RouterModule } from '@angular/router';
 import { AngularFireModule } from '@angular/fire/compat';
 import { RootComponent } from './root/root.component';
 import { LoginComponent } from './login/login.component';
+import { ListProdutoComponent } from './list-produto/list-produto.component';
 import { AuthService } from './auth.service';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhN8CaCFElgAIE238E_LCeWtGyLyQxMSU",
-  authDomain: "angular-project-3558c.firebaseapp.com",
-  databaseURL: "https://angular-project-3558c-default-rtdb.firebaseio.com",
-  projectId: "angular-project-3558c",
-  storageBucket: "angular-project-3558c.appspot.com",
-  messagingSenderId: "240526118246",
-  appId: "1:240526118246:web:6c4b0e1547c5917b940ca7",
-  measurementId: "G-6HS4Z489ME"
+  apiKey: 'AIzaSyBhN8CaCFElgAIE238E_LCeWtGyLyQxMSU',
+  authDomain: 'angular-project-3558c.firebaseapp.com',
+  databaseURL: 'https://angular-project-3558c-default-rtdb.firebaseio.com',
+  projectId: 'angular-project-3558c',
+  storageBucket: 'angular-project-3558c.appspot.com',
+  messagingSenderId: '240526118246',
+  appId: '1:240526118246:web:6c4b0e1547c5917b940ca7',
+  measurementId: 'G-6HS4Z489ME',
 };
 
 @NgModule({
@@ -26,7 +27,7 @@ const firebaseConfig = {
     FormsModule,
     AngularFireModule.initializeApp(firebaseConfig),
   ],
-  declarations: [RootComponent, LoginComponent],
+  declarations: [RootComponent, LoginComponent, ListProdutoComponent],
   bootstrap: [RootComponent],
   providers: [AuthService],
 })
