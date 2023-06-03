e154eb0af0d1498ef22553178841b970c22716e3
Daniel-Mingoranse <50594387+Daniel-Mingoranse@users.noreply.github.com>
2023-06-02 23:01:34 -0300

    Desevolvimento funcionalidade de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,17 @@ import { RouterModule } from '@angular/router';
 import { AngularFireModule } from '@angular/fire/compat';
 import { RootComponent } from './root/root.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBhN8CaCFElgAIE238E_LCeWtGyLyQxMSU',
-  authDomain: 'angular-project-3558c.firebaseapp.com',
-  databaseURL: 'https://angular-project-3558c-default-rtdb.firebaseio.com',
-  projectId: 'angular-project-3558c',
-  storageBucket: 'angular-project-3558c.appspot.com',
-  messagingSenderId: '240526118246',
-  appId: '1:240526118246:web:6c4b0e1547c5917b940ca7',
-  measurementId: 'G-6HS4Z489ME',
+  apiKey: "AIzaSyBhN8CaCFElgAIE238E_LCeWtGyLyQxMSU",
+  authDomain: "angular-project-3558c.firebaseapp.com",
+  databaseURL: "https://angular-project-3558c-default-rtdb.firebaseio.com",
+  projectId: "angular-project-3558c",
+  storageBucket: "angular-project-3558c.appspot.com",
+  messagingSenderId: "240526118246",
+  appId: "1:240526118246:web:6c4b0e1547c5917b940ca7",
+  measurementId: "G-6HS4Z489ME"
 };
 
 @NgModule({
@@ -27,5 +28,6 @@ const firebaseConfig = {
   ],
   declarations: [RootComponent, LoginComponent],
   bootstrap: [RootComponent],
+  providers: [AuthService],
 })
 export class AppModule {}
